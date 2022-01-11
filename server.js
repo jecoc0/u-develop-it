91e55b27e3693873ab0c59cd6f449d8b2476cf0b
Jessica <sevorg.jess@gmail.com>
2022-01-10 22:34:11 -0700

    SQL Query and express routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,8 +8,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '',
+    database: 'election'
+  },
+  console.log('Connected to the election database')
+)
 
-// default response for any other request (Not found)
+// query the database to test the connection
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
+// default response for any other request (Not found) - "catchall"
 app.use((req, res) => {
   res.status(404).end();
 });
